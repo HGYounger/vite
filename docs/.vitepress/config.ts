f97614f42be2cf6a1f9f23d4a3b8e5e123065ff1
huangguangyang <hgy2333@qq.com>
2023-07-19 10:34:36 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+import { SearchPlugin } from "vitepress-plugin-search"
+
+var options = {
+  previewLength: 62,
+  buttonLabel: "Search",
+  placeholder: "请输入搜索内容",
+  allow: [],
+  ignore: [],
+};
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "huangguangyang 专属站点",
+  description: "666",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/avatar.jpeg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+  },
+  lastUpdated: true,
+  vite: { plugins: [SearchPlugin(options)] }
+})
